0b786cf208b7a4f044d8f627e9583832a454b5dc
Ash <ashlegitimate@outlook.com>
2025-05-14 14:36:41 -0400

    ChessBoard for now.

---
+++ ChessBoard.cpp
@@ -0,0 +1,54 @@
+#include "ChessBoard.h"
+#include <iostream>
+
+ChessBoard::ChessBoard(){
+
+    //2D dynamic arr where ea element is smart pointer pointing at chesspiece
+    board.resize(BOARD_SIZE, std::vector<std::unique_ptr<ChessPiece>>(BOARD_SIZE, nullptr));
+    initializeBoard();
+}
+
+void ChessBoard::initializeBoard(){
+    //Positioning for black piece
+    //Creating unique pointer to point to each piece object and assigning to initial position.
+    board[0][0] = std::make_unique<Rook>(Color::Black);
+    board[0][1] = std::make_unique<Knight>(Color::Black);
+    board[0][2] = std::make_unique<Bishop>(Color::Black);
+    board[0][3] = std::make_unique<Queen>(Color::Black);
+    board[0][4] = std::make_unique<King>(Color::Black);
+    board[0][5] = std::make_unique<Bishop>(Color::Black);
+    board[0][6] = std::make_unique<Knight>(Color::Black);
+    board[0][7] = std::make_unique<Rook>(Color::Black);
+    //Looping for all pawns
+    for (int j = 0; j < BOARD_SIZE; j++) {
+        board[1][j] = std::make_unique<Pawn>(Color::Black);
+    }
+    
+    //Positioning for white pieces.
+    board[7][0] = std::make_unique<Rook>(Color::White);
+    board[7][1] = std::make_unique<Knight>(Color::White);
+    board[7][2] = std::make_unique<Bishop>(Color::White);
+    board[7][3] = std::make_unique<Queen>(Color::White);
+    board[7][4] = std::make_unique<King>(Color::White);
+    board[7][5] = std::make_unique<Bishop>(Color::White);
+    board[7][6] = std::make_unique<Knight>(Color::White);
+    board[7][7] = std::make_unique<Rook>(Color::White);
+    for (int j = 0; j < BOARD_SIZE; j++) {
+        board[6][j] = std::make_unique<Pawn>(Color::White);
+    }
+
+}
+
+void ChessBoard::printBoard() const {
+    //Loop through the board rows.
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        //Loop through the board columns.
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            if (board[i][j])
+                std::cout << board[i][j]->getSymbol() << " ";
+            else
+                std::cout << "X ";
+        }
+        std::cout << std::endl;
+    }
+}
--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -13,38 +13,38 @@ Color ChessPiece::getColor() const{
 
 //Derived Classes
 
-// Pawn implementation
+//Pawn implementation
 //Virtual fnc return P for White small p for black
 Pawn::Pawn(Color c) : ChessPiece(c){}
 char Pawn::getSymbol() const{
     return (c == Color::White ? 'P': 'p');
 }
 
-// Rook implementation
+//Rook implementation
 Rook::Rook(Color c) : ChessPiece(c) { }
 char Rook::getSymbol() const {
     return (c == Color::White) ? 'R' : 'r';
 }
 
-// Knight implementation
+//Knight implementation
 Knight::Knight(Color c) : ChessPiece(c) { }
 char Knight::getSymbol() const {
     return (c == Color::White) ? 'N' : 'n';
 }
 
-// Bishop implementation
+//Bishop implementation
 Bishop::Bishop(Color c) : ChessPiece(c) { }
 char Bishop::getSymbol() const {
     return (c == Color::White) ? 'B' : 'b';
 }
 
-// Queen implementation
+//Queen implementation
 Queen::Queen(Color c) : ChessPiece(c) { }
 char Queen::getSymbol() const {
     return (c == Color::White) ? 'Q' : 'q';
 }
 
-// King implementation
+//King implementation
 King::King(Color c) : ChessPiece(c) { }
 char King::getSymbol() const {
     return (c == Color::White) ? 'K' : 'k';
