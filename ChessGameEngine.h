47e68868aa3a97e844f3c9eb7ef009163ffc22a8
Ash <ashlegitimate@outlook.com>
2025-06-05 18:04:00 -0400

    GameEng .h for now

--- ChessGameEngine.h
+++ ChessGameEngine.h
@@ -1,2 +1,28 @@
-#include <iostream>
-using namespace std;
+#ifndef CHESSGAMEENGINE_H
+#define CHESSGAMEENGINE_H
+
+#include "ChessBoard.h"
+#include "ChessMoves.h"
+
+class ChessGameEngine {
+public:
+    ChessGameEngine();
+
+    //Making a move
+    bool makeMove(const ChessMoves& move);
+
+    //Displaying the game
+    void displayGame() const;
+
+private:
+    ChessBoard board;
+    Color currentTurn;
+
+    //Move validation
+    bool isMoveLegal(const ChessMoves& move) const;
+
+    //Changing turns
+    void switchTurn();
+};
+
+#endif
